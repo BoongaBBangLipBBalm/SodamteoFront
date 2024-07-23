912dc99db3b10e5f22c69fad75b93c2cbbfc276f
enunsnv <seoej0315@naver.com>
2024-07-23 14:22:34 +0900

    🚚 route: redirect / -> /intro

--- next.config.mjs
+++ next.config.mjs
@@ -1,4 +1,24 @@
 /** @type {import('next').NextConfig} */
-const nextConfig = {};
+const nextConfig = {
+  async redirects() {
+    return [
+      {
+        source: '/', 
+        destination: '/intro',
+        permanent: true
+      }
+    ];
+  },
+
+  async rewrites() {
+    return [
+      {
+        source: '/api/:path*',
+        destination: 'https://port-0-aecofarm-lyhj20nc49bb1c32.sel5.cloudtype.app/:path*',
+      },
+    ]
+  },
+
+};
 
 export default nextConfig;
---
+++ src/app/intro/page.tsx
@@ -0,0 +1,13 @@
+import Head from 'next/head';
+
+const Home = () => {
+  return (
+    <div>
+      <main>
+       하이
+      </main>
+    </div>
+  );
+};
+
+export default Home;
